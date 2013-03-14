d97c10c8f05ac7f58efad4aaba2c3e359598394a
abekim <abekim0607@gmail.com>
2013-03-13 23:24:17 -0400

    oops. forgot a view for the app itself

---
+++ views/appView.js
@@ -0,0 +1,21 @@
+var app = app || {} //global app
+
+$(function ($) {
+  'use strict';
+
+  app.AppView = Backbone.View.extend({
+
+    el: '#container',
+
+    //events to look out for
+    events: {},
+
+    initialize: function () {
+      //initialize
+    },
+
+    render: function () {
+      //render stuff here.
+    }
+  });
+});
